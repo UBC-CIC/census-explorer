85227b23c26565358bebbc99d4124545911e0f9f
da Costa <alyssa.dacosta@ubc.ca>
2021-08-03 10:33:04 -0700

    Fetch data from CloudFront

--- api.py
+++ api.py
@@ -1,18 +1,27 @@
 from flask import Flask
 
+import pandas as pd
+import requests
+
 app = Flask(__name__)
 
+cloudfront = 'https://d16fwanmhq4nj5.cloudfront.net'
+
+# note: this takes about 25s to get the data
 @app.route('/api/census-data')
 def get_census_data():
-    # TODO
-    return {'census-data': 'census data test'}
+    # fetch data from AWS S3 via CloudFront
+    data_loc = cloudfront + '/censusdata_reformatted.json'
+    return requests.get(url=data_loc).content
 
 @app.route('/api/family-data')
 def get_family_data():
-    # TODO
-    return {'family-data': 'family data test'}
+    # fetch data from AWS S3 via CloudFront
+    data_loc = cloudfront + '/familydonationdata2016_reformatted.json'
+    return requests.get(url=data_loc).content
 
 @app.route('/api/income-data')
 def get_income_data():
-    # TODO
-    return {'income-data': 'income data test'}
+    # fetch data from AWS S3 via CloudFront
+    data_loc = cloudfront +'/incomedonationdata2016_reformatted.json'
+    return requests.get(url=data_loc).content
