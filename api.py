8870b5fc7f07436dc0882c014f41067587809300
da Costa <alyssa.dacosta@ubc.ca>
2021-08-09 15:30:18 -0700

    Fetch census data by province

--- api.py
+++ api.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask
+from flask import Flask, request
 
 os.system('pip install requests')
 import requests
@@ -8,21 +8,21 @@ app = Flask(__name__)
 
 cloudfront = 'https://d16fwanmhq4nj5.cloudfront.net'
 
-# note: this takes about 15s to get the data
 # note: this is for the creation of the map, NOT user download
-@app.route('/api/census-data')
-def get_census_data():
+# use province abbreviations: i.e. AB, BC, etc. (can also use CAN)
+@app.route('/api/census-data/<province>', methods=['GET'])
+def get_census_data(province):
     # fetch data from AWS S3 via CloudFront
-    data_loc = cloudfront + '/mappingcensusdata_reformatted.json'
+    data_loc = cloudfront + '/mappingcensusdata-' + province + '_reformatted.json'
     return requests.get(url=data_loc).content
 
-@app.route('/api/family-data')
+@app.route('/api/family-data', methods=['GET'])
 def get_family_data():
     # fetch data from AWS S3 via CloudFront
     data_loc = cloudfront + '/familydonationdata2016_reformatted.json'
     return requests.get(url=data_loc).content
 
-@app.route('/api/income-data')
+@app.route('/api/income-data', methods=['GET'])
 def get_income_data():
     # fetch data from AWS S3 via CloudFront
     data_loc = cloudfront +'/incomedonationdata2016_reformatted.json'
