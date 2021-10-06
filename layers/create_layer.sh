ce4c9c4a1d54ff6cfa3ead67b8af92a07a308923
alyssadacosta <54870218+alyssadacosta@users.noreply.github.com>
2021-10-06 15:19:10 -0700

    Update create_layer.sh

--- layers/create_layer.sh
+++ layers/create_layer.sh
@@ -1,6 +1,3 @@
 read curdir <<< $(pwd)
 docker build --tag layer:latest .
-#docker run --rm -v "C:\Users\dacost2\Desktop\census-explorer-backend\layers":/dest layer:latest cp code.zip /dest/lambda_layer.zip
-
-docker run -v "C:\Users\dacost2\Desktop\census-explorer-backend\layers":/dest layer:latest cp code.zip ./lambda_layer.zip
-#docker run -v "C:\Users\dacost2\Desktop\census-explorer-backend\layers":/dest layer:latest ls
+docker run --rm -v ${curdir}:/dest layer:latest cp code.zip /dest/lambda_layer.zip
